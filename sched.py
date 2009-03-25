ab4bb6633fa90bda548e609a48a4c3e624ea7338
hugh.chaffey-millar <hugh.chaffey-millar@ch.tum.de>
2009-03-25 17:39:52 +0000

    daily

--- sched.py
+++ sched.py
@@ -1,11 +1,15 @@
 class ParaSched(threading.Thread):
-    def __init__(self, total_procs, norm_procs, max_procs, input_list):
+    def __init__(self, total_procs, norm_procs, max_procs, input_list, f):
         self.__total_procs = total_procs
         self.__norm_procs = norm_procs
         self.__max_procs = norm_procs
+
+        # setup queue
         self.__queue = Queue()
         for x in input_list:
             self.__queue.put(x)
 
         self.__results = []
+
+        # start workers
 
