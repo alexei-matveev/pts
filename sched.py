4ab975ae5ae8b5395208225dc553876bb7476ba4
hugh.chaffey-millar <hugh.chaffey-millar@ch.tum.de>
2009-04-28 13:33:27 +0000

    more testing and therefore correct implementation of scheduler/calculation manager

--- sched.py
+++ sched.py
@@ -3,42 +3,26 @@ from thread import *
 from Queue import *
 from numpy import *
 
+# setup logging
+import logging
+print "Defining logger"
+lg = logging.getLogger(__name__)
+lg.setLevel(logging.DEBUG)
+
+if not globals().has_key("lg"):
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    lg.addHandler(ch)
+formatter = logging.Formatter("%(name)s (%(levelname)s): %(message)s")
+ch.setFormatter(formatter)
+
 
 SAMENESS_THRESH = 1e-6
 def is_same_v(v1, v2):
     return linalg.norm(v1 - v2) < SAMENESS_THRESH
+def is_same_e(e1, e2):
+    return abs(e1 - e2) < SAMENESS_THRESH
 
-def test_threads():
-    def worker(q):
-        print "worker started"
-        while not q.empty():
-            import subprocess
-            import os
-
-            item = q.get()
-            p = subprocess.Popen("g03 " + item, shell=True)
-            sts = os.waitpid(p.pid, 0)
-
-            my_id = get_ident()
-            print "thread " + str(my_id) + ": item " + item + " complete"
-            q.task_done()
-
-    queue = Queue()
-    
-    items = ["j1.com", "j2.com", "j3.com", "j4.com", "j5.com", "j6.com", "j7.com"]
-    for item in items:
-        print "Adding ", item
-        queue.put(item)
-
-    no_of_workers = 3
-    for i in range(no_of_workers):
-         t = Thread(target=worker, args=(queue,))
-         t.start()
-
-    if queue.empty():
-        print "Problem: queue was empty"
-    else:
-        queue.join()
 
 class MiniQC():
     """Mini qc driver. Just or testing"""
@@ -61,6 +45,7 @@ def test_calcManager():
 
     tmp = array((0.5, 0.7))
     inputs = [tmp * x for x in range(10)]
+    lg.info("inputs are %s", inputs)
 
     for i in range(len(inputs)):
         cm.request_energy(inputs[i])
@@ -69,22 +54,49 @@ def test_calcManager():
         if i > 3:
             cm.request_grad(inputs[i])
 
-    print cm
     cm.proc_requests()
 
     for i in inputs:
-        print "Vector ", i,
+        lg.info("Vector %s" % i)
         try:
             e = cm.energy(i)
-            print "e =", e,
+            lg.info("e = %s" % e)
         except Exception, err:
-            print err
+            lg.error(err)
 
         try:
             g = cm.grad(i)
-            print "g =", g
+            lg.info("g = %s" % g)
         except Exception, err:
-           print err
+           lg.error(err)
+
+    # tests caching of previously calculated results
+    lg.info("Starting tests of result caching")
+    inputs = [array((20.,30.))] + inputs
+    print "inputs =", inputs
+    for i in range(len(inputs))[:-3]:
+        cm.request_energy(inputs[i])
+        if i < 7:
+            cm.request_energy(inputs[i])
+        if i > 8:
+            cm.request_grad(inputs[i])
+
+    cm.proc_requests()
+
+    for i in inputs:
+        lg.info("Vector %s" % i)
+        try:
+            e = cm.energy(i)
+            lg.info("e = %s" % e)
+        except Exception, err:
+            lg.error(err)
+
+        try:
+            g = cm.grad(i)
+            lg.info("g = %s" % g)
+        except Exception, err:
+           lg.error(err)
+
 
 class CalcManager():
     """
@@ -125,20 +137,31 @@ class CalcManager():
     def request_job(self, v, type):
         """Place into queue, request for calculations of type 'type'."""
 
-        for i in range(len(self.__pending_jobs)):
-#            print i, len(self.__pending_jobs)
-            j = self.__pending_jobs[i]
-            if j.geom_is(v):
-                j.add_calc(type)
-                return
+        result = self.__result_dict.get(v)
 
-        self.__pending_jobs.append(Job(v, type))
-        print self.__pending_jobs[-1]
+        # calculation not already performed
+        if result == None: 
+            # check jobs already in list
+            for i in range(len(self.__pending_jobs)):
+                j = self.__pending_jobs[i]
+                if j.geom_is(v):
+                    j.add_calc(type)
+                    return
+
+            # calc is not already in list so must add
+            self.__pending_jobs.append(Job(v, type))
+            lg.info("Requesting calc for %s of type %s" % (v, type))
+
+        # calculation has aleady been performed, will use cached version
+        elif result.has_field(type):
+            lg.info("Already have result for %s, will used cached value" % v)
+            return
 
     def proc_requests(self):
         """Process all jobs in queue."""
+        lg.info(self.__class__.__name__ + " %s jobs in queue" % len(self.__pending_jobs))
         if self.__para_sched != None:
-            print self.__class__.__name__ + " operating in parallel"
+            lg.info(self.__class__.__name__ + " operating in parallel")
             self.__para_sched.run_all(self.__pending_jobs)
 
             for r in self.__para_sched.get_results():
@@ -146,7 +169,7 @@ class CalcManager():
 
         # running serially
         else:
-            print self.__class__.__name__ + " operating serially"
+            lg.info(self.__class__.__name__ + " operating serially")
 
             for j in self.__pending_jobs:
                 res = self.qc_driver.run(j)
@@ -213,7 +236,6 @@ class ResultDict():
     def __init__(self, parent):
         self.list = []
         self.parent = parent
-        self.__name__ = "ResultDict"
 
     def add(self, v, res):
         """Add result res for vector v to the dictionary."""
@@ -259,16 +281,19 @@ class Result():
         s = self.__class__.__name__ + ": " + str(self.v) + " E = " + str(self.e) + " G = " + str(self.g)
         return s
 
+    def has_field(self, type):
+        return type == Job.G() and self.g != None \
+            or type == Job.E() and self.e != None
+        
     def merge(self, res):
-        assert self.is_same_v(self.v, res.v)
-        assert self.is_same_e(self.e, res.e)
+        assert is_same_v(self.v, res.v)
+        assert is_same_e(self.e, res.e)
 
         if self.g == None:
             self.g = res.g
         else:
             raise Exception("Trying to add a gradient result when one already exists")
         
-
 class ParaSched:
     def __init__(self, qc_driver, total_procs = 4, min_job_procs = 1, max_job_procs = 2):
         
@@ -282,40 +307,38 @@ class ParaSched:
     def __worker(self, pending, finished):
 
         my_id = get_ident()
-        print "worker started, id =", my_id
+        lg.debug("worker starting, id = %s" % my_id)
         while not pending.empty():
 
             try:
                 item = self.__pending.get()
-                res = self.__qc_driver.run(item)
-                finished.put(res)
 
-                print "thread " + str(my_id) + ": item " + str(item) + " complete"
+            except Queue.Empty, ex:
+                lg.error("Thrown by worker: " + str(my_id) + " " + str(ex))
+                return
 
-            except Exception, ex:
-                print "Thrown by worker: " + str(my_id) + " " + str(ex)
+            # call quantum chem driver
+            res = self.__qc_driver.run(item)
 
-            finally:
-                self.__pending.task_done()
+            finished.put(res)
+            self.__pending.task_done()
+            lg.debug("thread " + str(my_id) + ": item " + str(item) + " complete")
 
-        print "worker finished, id =", my_id
+        lg.debug("worker exiting, id = %s" % my_id)
 
     def run_all(self, jobs_list):
         """Start threads to process jobs in queue."""
 
         # place jobs in a queue
-        for j in jobs_list:
-            self.__pending.put(j)
+        while len(jobs_list) > 0:
+            self.__pending.put(jobs_list.pop())
 
         # start workers
-        print "%s spawning %d worker threads" % (self.__class__.__name__, self.__workers_count)
+        lg.info("%s spawning %d worker threads" % (self.__class__.__name__, self.__workers_count))
         for i in range(self.__workers_count):
             t = Thread(target=self.__worker, args=(self.__pending, self.__finished))
             t.start()
 
-        """if self.__pending.empty():
-            print "Problem: queue was empty"
-        else:"""
         self.__pending.join()
 
     def get_results(self):
@@ -325,4 +348,37 @@ class ParaSched:
             self.__finished.task_done()
 
         return results
+
+def test_threads():
+    def worker(q):
+        logging.info("worker started")
+        while not q.empty():
+            import subprocess
+            import os
+
+            item = q.get()
+            p = subprocess.Popen("g03 " + item, shell=True)
+            sts = os.waitpid(p.pid, 0)
+
+            my_id = get_ident()
+            print "thread " + str(my_id) + ": item " + item + " complete"
+            q.task_done()
+
+    queue = Queue()
+    
+    items = ["j1.com", "j2.com", "j3.com", "j4.com", "j5.com", "j6.com", "j7.com"]
+    for item in items:
+        print "Adding ", item
+        queue.put(item)
+
+    no_of_workers = 3
+    for i in range(no_of_workers):
+         t = Thread(target=worker, args=(queue,))
+         t.start()
+
+    if queue.empty():
+        print "Problem: queue was empty"
+    else:
+        queue.join()
+
 
