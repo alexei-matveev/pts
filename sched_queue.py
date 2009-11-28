e7791ab709e67fb14b74b96ec72c3c0284d14855
hugh.chaffey-millar <hugh.chaffey-millar@ch.tum.de>
2009-11-28 17:22:35 +0000

    daily, work done on Topology, doctests added.

--- sched_queue.py
+++ sched_queue.py
@@ -63,14 +63,85 @@ class Topology(object):
     Contents e.g.:
         [n]         Single system image, n processors.
         [a,b,c]     Multiple system images, each with a,b,c processors.
+
+
+    >>> t = Topology([4])
+    >>> t.get_range(2)
+    (array([0, 1]), 0, array([0, 1]), 1)
+
+    >>> print t
+    [2] / [4] / [[False, False, True, True]]
+
+    >>> t.get_range(2)
+    (array([2, 3]), 0, array([2, 3]), 2)
+
+    >>> print t
+    [0] / [4] / [[False, False, False, False]]
+
+    >>> t.put_range(2)
+    >>> print t
+    [2] / [4] / [[False, False, True, True]]
+
+
+
+    >>> t = Topology([1,1])
+    >>> t.get_range(2)
+    >>> t.get_range(1)
+    (array([0]), 0, array([0]), 1)
+    >>> t.get_range(1)
+    (array([1]), 1, array([0]), 2)
+    >>> t.get_range(1)
+    >>> print t
+    [0, 0] / [1, 1] / [[False], [False]]
+
+    >>> t = Topology(range(1,5))
+    >>> t.get_range(2)
+    (array([1, 2]), 1, array([0, 1]), 1)
+    >>> print t
+    [1, 0, 3, 4] / [1, 2, 3, 4] / [[True], [False, False], [True, True, True], [True, True, True, True]]
+
+    >>> t.get_range(1)
+    (array([0]), 0, array([0]), 2)
+
+    >>> t.get_range(1)
+    (array([6]), 3, array([0]), 3)
+
+    >>> t.get_range(1)
+    (array([3]), 2, array([0]), 4)
+
+    >>> t.get_range(2)
+    (array([4, 5]), 2, array([1, 2]), 5)
+
+
+    >>> t2 = t.copy()
+
+    >>> for i in [1, 2, 3, 4, 5]:
+    ...     t2.put_range(i)
+    >>> print t2
+    [1, 2, 3, 4] / [1, 2, 3, 4] / [[True], [True, True], [True, True, True], [True, True, True, True]]
+
+
+
+
     """
     def __init__(self, shape):
         self.state = []
         for i in shape:
-            self.state.append([False for j in range(i)])
+            assert i > 0
+            self.state.append([True for j in range(i)])
+
+        self.id = 0
+        self._alloc = dict()
+
+        #TODO: create lock
+
+
+    def __str__(self):
+        msg = "%s / %s / %s" % (self.available, self.all, self.state)
+        return msg
 
     @property
-    def filled(self):
+    def available(self):
         return [sum(i) for i in self.state]
 
     @property
@@ -78,24 +149,65 @@ class Topology(object):
         return [len(i) for i in self.state]
 
     
+    def put_range(self, id):
+        """Relinquish ownership of cpus allocated with id."""
+
+        ix_part, ixs_local = self._alloc.pop(id)
+        part = self.state[ix_part]
+        for i in ixs_local:
+            assert not part[i]
+            part[i] = True
+        
     def get_range(self, n):
         """Try to find a range of n cpus in the system.
         
-        Returns the range that maximises the number of leftover cpus in the 
+        If possible, (A) returns cpus in a partition of exactly n cpus, otherwise
+        (B) returns cpus in a partition with exactly n remaining cpus, otherwise
+        (C) returns the range that maximises the number of leftover cpus in the 
         partition.
         """
 
-        diffs = np.array([N - n for N in self.filled])
-        partition_ix = diffs.argmax()
+        assert n > 0
 
-        mask = self.state[partition_ix]
+        if n in self.available:
+            if n in self.all and self.all.index(n) == self.available.index(n):
+                ix_part = self.all.index(n)
+            
+            else:
+                ix_part = self.available.index(n)
+
+        else:
+            diffs = np.array([N - n for N in self.available])
+
+            if diffs.max() < 0:
+                return None
+
+            ix_part = diffs.argmax()
+
+
+        mask = self.state[ix_part]
         assert sum(mask) >= n
 
-        local_range = np.array(self.get_frees(mask)[:n])
+        ixs_local = np.array(self.frees(mask)[:n])
 
-        global_range = local_range + sum(self.all[:partition_ix+1])
+        ix_global_start = sum(self.all[:ix_part])
 
-        return global_range
+        ixs_global = ixs_local + ix_global_start
+
+        for i in ixs_local:
+            self.state[ix_part][i] = False
+
+
+        # Keep a record of allocation so that cpus can be relinquished just
+        # by quoting the id.
+        self.id += 1
+        self._alloc[self.id] = (ix_part, ixs_local)
+        return (ixs_global, ix_part, ixs_local, self.id)
+
+    def frees(self, mask):
+        """Returns indices of all free cpus in a mask."""
+
+        return [i for i in range(len(mask)) if mask[i]]
 
     def calc_used(self, task_cpus):
         """Finds the number of used processors when running jobs 
@@ -117,15 +229,13 @@ class Topology(object):
 
         assert used <= sum(self.all)
         return used
-
-    def get_free_cpus(self):
-        """Return a list of the total number of free slots in each system partition."""
-
-        return self.all - self.filled
-
-    #TODO: define getters and setters?
+    
+    def copy(self):
+        from copy import deepcopy
+        return deepcopy(self)
 
 class SchedQueue(Queue.Queue):
+    """A thread-safe queue object that comes annotates it's contents with scheduling information."""
 
     _sleeptime = 0.3
 
@@ -146,3 +256,8 @@ class SchedQueue(Queue.Queue):
             time.sleep(self._sleeptime)
 
         return Queue.Queue.get(self, item)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
