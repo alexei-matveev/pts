a64b98c2fdb57d2d9e6410ff326bb38fe7a6a255
hugh.chaffey-millar <hugh.chaffey-millar@ch.tum.de>
2009-12-14 12:27:47 +0000

    sub-daily

---
+++ optwrap.py
@@ -0,0 +1,52 @@
+"""Provides a uniform interface to a variety of optimisers."""
+
+from scipy.optimize.lbfgsb import fmin_l_bfgs_b
+from scipy.optimize import fmin_cg
+
+import ase
+import aof
+
+__all__ = ["opt"]
+
+def runopt(name, CoS, tol, maxit, callback, maxstep=0.2):
+    names = ['scipy_lbfgsb', 'ase_lbfgs', 'ase_fire', 'quadratic_string', 'scipy_cg']
+    if name == 'scipy_lbfgsb':
+        opt, energy, dict = fmin_l_bfgs_b(CoS.obj_func,
+                                  CoS.get_state_as_array(),
+                                  fprime=CoS.obj_func_grad,
+                                  callback=callback,
+                                  pgtol=tol,
+                                  maxfun=maxit)
+        return dict
+
+    elif name == 'scipy_cg':
+        opt, dict = fmin_cg(CoS.obj_func,
+                                  CoS.get_state_as_array(),
+                                  fprime=CoS.obj_func_grad,
+                                  callback=callback,
+                                  gtol=tol,
+                                  maxiter=maxit)
+        return dict
+
+
+    elif name[0:4] == 'ase_':
+
+        if name == 'ase_lbfgs':
+            opt = ase.LBFGS(CoS, maxstep=maxstep)
+        elif name == 'ase_fire':
+            opt = ase.FIRE(CoS)
+        else:
+            assert False, ' '.join(["Unrecognised algorithm", name, "not in"] + names)
+
+        # attach optimiser to print out each step in
+        opt.attach(lambda: callback(None), interval=5)
+        opt.run(fmax=tol)
+        x_opt = CoS.state_vec
+        return None
+
+    elif name == 'quadratic_string':
+        gqs = aof.searcher.QuadraticStringMethod(CoS, callback=callback, update_trust_rads = True)
+        opt = gqs.opt()
+    else:
+        assert False, ' '.join(["Unrecognised algorithm", name, "not in"] + names)
+
