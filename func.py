8c76f87c897232b694ce88f6f54b833592705074
matveev <matveev@theochem.tu-muenchen.de>
2010-01-26 17:30:08 +0000

    resolution: rm trailing spaces and add fprimeprime/stat_points to CubicFunc

--- func.py
+++ func.py
@@ -285,7 +285,25 @@ class CubicFunc(Func):
 
     def fprime(self, x):
         return dot(array((3*x**2, 2*x, 1., 0.)), self.coeffs)
-               
+
+    def fprimeprime(self, x):
+        return dot(array((6*x, 2, 0., 0.)), self.coeffs)
+
+    def stat_points(self):
+        """Returns the locations of the stationary points."""
+#        assert False, "Function stat_points untested."
+        quadratic_coeffs = array((3, 2, 1.)) * self.coeffs[:3]
+
+        a,b,c = quadratic_coeffs
+
+        delta = b**2 - 4*a*c
+        if delta < 0:
+            return []
+        elif delta == 0:
+            return [-b / 2 / a]
+        else:
+            return [(-b + delta**0.5) / 2 / a, (-b - delta**0.5) / 2 / a]
+
 
 class SplineFunc(Func):
     def __init__(self, xs, ys):
