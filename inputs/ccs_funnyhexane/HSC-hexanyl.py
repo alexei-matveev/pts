1f7a38a5b95e8174a8ab2ea74906acb9006e29a0
hugh.chaffey-millar <hugh.chaffey-millar@ch.tum.de>
2010-02-15 16:43:49 +0000

    Added TS search example for ComplexCoordSys

---
+++ inputs/ccs_funnyhexane/HSC-hexanyl.py
@@ -0,0 +1,16 @@
+zmts = """C
+S 1 var1
+H 1 var2 2 var3
+
+var1    1.0
+var2    1.0
+var3    1.0
+"""
+
+xyz_raw = common.file2str('HSC-hexanyl.geom.t')
+xyzs = '\n'.join(xyz_raw.splitlines()[:-3])
+xyz = XYZ(xyzs)
+zmt = ZMatrix2(zmts, RotAndTrans())
+
+ccs = ccsspec([xyz, zmt], carts=XYZ(xyz_raw))
+
