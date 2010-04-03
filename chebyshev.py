f7e52e6698a3d77a539de8536102b47bbda4de31
matveev <matveev@theochem.tu-muenchen.de>
2010-04-03 20:46:22 +0000

    AOF: make Chebyshev a Func

--- chebyshev.py
+++ chebyshev.py
@@ -1,17 +1,18 @@
 from numpy import pi, cos
+from func import Func
 
-class Chebyshev:
+class Chebyshev(Func):
     """
-    Chebyshev(a, b, n, func)
-    Given a function func, lower and upper limits of the interval [a,b],
+    Chebyshev(a, b, n, fun)
+    Given a function fun, lower and upper limits of the interval [a,b],
     and maximum degree n, this class computes a Chebyshev approximation
     of the function.
     Method eval(x) yields the approximated function value.
 
 
         >>> from numpy import sin
-        >>> ch = Chebyshev(0, pi / 12, 8, sin)
-        >>> ch.eval(0.1)
+        >>> s = Chebyshev(0, pi / 12, 8, sin)
+        >>> s(0.1)
         0.099833416646828876
         >>> sin(0.1)
         0.099833416646828155
@@ -19,18 +20,18 @@ class Chebyshev:
     See Numerical Recepies or
     http://www.excamera.com/sphinx/article-chebyshev.html
     """
-    def __init__(self, a, b, n, func):
+    def __init__(self, a, b, n, fun):
         self.a = a
         self.b = b
-        self.func = func
+        self.fun = fun
 
         bma = 0.5 * (b - a)
         bpa = 0.5 * (b + a)
-        f = [func(cos(pi * (k + 0.5) / n) * bma + bpa) for k in range(n)]
+        fs = [fun(cos(pi * (k + 0.5) / n) * bma + bpa) for k in range(n)]
         fac = 2.0 / n
-        self.c = [fac * sum([f[k] * cos(pi * j * (k + 0.5) / n) for k in range(n)]) for j in range(n)]
+        self.c = [fac * sum([fs[k] * cos(pi * j * (k + 0.5) / n) for k in range(n)]) for j in range(n)]
 
-    def eval(self, x):
+    def f(self, x):
         a,b = self.a, self.b
         assert(a <= x <= b)
         y = (2.0 * x - a - b) * (1.0 / (b - a))
