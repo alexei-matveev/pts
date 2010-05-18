c8b9e0e6c776cb125f1b61a4a93f39db02909cdd
matveev <matveev@theochem.tu-muenchen.de>
2010-05-18 16:28:47 +0000

    AOF: fix first-level indent to 4 spaces

--- vib.py
+++ vib.py
@@ -244,143 +244,143 @@ def derivatef( g0, x0, delta = 0.01, p_map = pool_map  , direction = 'central' )
     return deriv
 
 def vibmodes(atoms, startdir = None, mask = None, workhere = False, storehessian = True, alsovec = False, **kwargs ):
-     """
-     Wrapper around vibmode, which used the atoms objects
-     The hessian is calculated by derivatef
-     qfunc.fwrapper is used as a wrapper to calulate the gradients
-     """
-     func = fwrapper(atoms, startdir = startdir, mask = mask, workhere = workhere)
-     xcenter = func.getpositionsfromatoms()
-     mass = func.getmassfromatoms()
-     # the derivatives are needed
-     hessian = derivatef( func.perform, xcenter,**kwargs )
-     if storehessian:
-         store_hessian(hessian)
-     freqs, modes = vibmod(mass, hessian)
-     output(freqs, modes, mass, alsovec)
+    """
+    Wrapper around vibmode, which used the atoms objects
+    The hessian is calculated by derivatef
+    qfunc.fwrapper is used as a wrapper to calulate the gradients
+    """
+    func = fwrapper(atoms, startdir = startdir, mask = mask, workhere = workhere)
+    xcenter = func.getpositionsfromatoms()
+    mass = func.getmassfromatoms()
+    # the derivatives are needed
+    hessian = derivatef( func.perform, xcenter,**kwargs )
+    if storehessian:
+        store_hessian(hessian)
+    freqs, modes = vibmod(mass, hessian)
+    output(freqs, modes, mass, alsovec)
 
 
 def store_hessian(hessian):
-     print "Vibration module: printing numerical calculated hessian:"
-     print hessian
-     wr = open("Hessian.dat","w").write
-     dim1 = len(hessian)
-     for i in range(dim1):
-         for j in range(dim1):
-             wr("%23.12f " % hessian[i,j])
-         wr("\n")
+    print "Vibration module: printing numerical calculated hessian:"
+    print hessian
+    wr = open("Hessian.dat","w").write
+    dim1 = len(hessian)
+    for i in range(dim1):
+        for j in range(dim1):
+            wr("%23.12f " % hessian[i,j])
+        wr("\n")
 
 def vibmod(mass, hessian):
-     """
-     calculates the vibration modes in harmonic approximation
+    """
+    calculates the vibration modes in harmonic approximation
 
-     atoms is the atom system (ase.atoms) with positions set to the geometry, on which the
-     frequencies are wanted
+    atoms is the atom system (ase.atoms) with positions set to the geometry, on which the
+    frequencies are wanted
 
-     func should do the gradient call on a given geometry
+    func should do the gradient call on a given geometry
 
-     if p_map is choosen as a parallel variant, all gradient calculations will be performed
-     in parallel
-     direction = 'central' should be the most accurate one but 'forward'/'backward' needs 
-     fewer calculations all together (but the results may be really much worse, compare the results
-     from the derivatef tests in the doctests.
-     delta is the defaultvalue for delta in the derivatef function
+    if p_map is choosen as a parallel variant, all gradient calculations will be performed
+    in parallel
+    direction = 'central' should be the most accurate one but 'forward'/'backward' needs 
+    fewer calculations all together (but the results may be really much worse, compare the results
+    from the derivatef tests in the doctests.
+    delta is the defaultvalue for delta in the derivatef function
 
-     alsovec says that not only the frequencies of the mode but also the eigenvectors are 
-     wanted
-     """
-     mass = asarray(mass)
+    alsovec says that not only the frequencies of the mode but also the eigenvectors are 
+    wanted
+    """
+    mass = asarray(mass)
 
-     # make sure that the hessian is hermitian:
-     hessian = 0.5 * (hessian + hessian.T)
+    # make sure that the hessian is hermitian:
+    hessian = 0.5 * (hessian + hessian.T)
 
-     # and also the massvec
-     mass = 0.5 * (mass + mass.T)
+    # and also the massvec
+    mass = 0.5 * (mass + mass.T)
 
-     # solve the generalized eigenvalue problem
-     #
-     #   w, V = geigs(H, M)
-     #
-     #   A * V[:, i] = w[i] * M * V[:, i]
-     #
-     # For symmetric H and symmetric M > 0 the eigenvalues are real
-     # and eigenvectors can be (and are) chosen orthogonal:
-     #
-     eigvalues, eigvectors = geigs(hessian, mass)
+    # solve the generalized eigenvalue problem
+    #
+    #   w, V = geigs(H, M)
+    #
+    #   A * V[:, i] = w[i] * M * V[:, i]
+    #
+    # For symmetric H and symmetric M > 0 the eigenvalues are real
+    # and eigenvectors can be (and are) chosen orthogonal:
+    #
+    eigvalues, eigvectors = geigs(hessian, mass)
 
-     #
-     # Negative eigenvalues correspond to imaginary frequencies,
-     # for square root to work lift the real numbers to complex domain:
-     #
-     freqs = sqrt(eigvalues + 0j)
-     modes = eigvectors.T
+    #
+    # Negative eigenvalues correspond to imaginary frequencies,
+    # for square root to work lift the real numbers to complex domain:
+    #
+    freqs = sqrt(eigvalues + 0j)
+    modes = eigvectors.T
 
-     return freqs, modes
+    return freqs, modes
 
 def output(freqs, eigvectors, mass, alsovec = False):
-     # scale eigenvalues in different units:
-     # E = hbar * omega [eV] = hvar * [1/s]
-     # omega = sqrt(H /m), [H] = [kJ/Ang^2] , [m] = [amu], [omega] = [1/s] = [ J/m^2 /kg]
-     scalfact = units._hbar * 1e10 / units.Ang * sqrt( units.kJ /( units._amu * 1000 ) )
-     modeenerg =  scalfact * freqs
-     modeincm  = modeenerg * units._e / units._c / units._hplanck * 0.01
-     print "===================================================="
-     print " Number  imag.   Energy in eV      Energy in cm^-1"
-     print "----------------------------------------------------"
-     for i, mode_e  in enumerate(modeenerg):
-           if mode_e.imag != 0 and abs(mode_e.imag) > abs(mode_e.real):
-               print "%3d       yes     %10.4f       %10.2f" % (i+1,  mode_e.imag, modeincm[i].imag)
-           else:
-               print "%3d       no      %10.4f       %10.2f" % (i+1,  mode_e.real, modeincm[i].real)
-     print "----------------------------------------------------"
+    # scale eigenvalues in different units:
+    # E = hbar * omega [eV] = hvar * [1/s]
+    # omega = sqrt(H /m), [H] = [kJ/Ang^2] , [m] = [amu], [omega] = [1/s] = [ J/m^2 /kg]
+    scalfact = units._hbar * 1e10 / units.Ang * sqrt( units.kJ /( units._amu * 1000 ) )
+    modeenerg =  scalfact * freqs
+    modeincm  = modeenerg * units._e / units._c / units._hplanck * 0.01
+    print "===================================================="
+    print " Number  imag.   Energy in eV      Energy in cm^-1"
+    print "----------------------------------------------------"
+    for i, mode_e  in enumerate(modeenerg):
+          if mode_e.imag != 0 and abs(mode_e.imag) > abs(mode_e.real):
+              print "%3d       yes     %10.4f       %10.2f" % (i+1,  mode_e.imag, modeincm[i].imag)
+          else:
+              print "%3d       no      %10.4f       %10.2f" % (i+1,  mode_e.real, modeincm[i].real)
+    print "----------------------------------------------------"
 
-     if (alsovec):
-          write = stdout.write
+    if (alsovec):
+         write = stdout.write
 
-          # we don't know if they are cartesian
-          print "The corresponding eigenvectors  are:"
-          print "Number   Vector"
-          for i, ev  in enumerate(eigvectors):
-               write("%3d :    " % (i+1)  )
-               for j in range(int(len(ev)/3)):
-                    for k in [0,1,2]:
-                        write("  %10.7f" % (ev[j * 3 + k]))
-                    write("\n         " )
-               for k in range(int(len(ev)/3)*3,len(ev)):
-                    write("  %10.7f" % (ev[k]))
-               write("\n")
-          print "----------------------------------------------------"
-          print "kinetic energy distribution"
-          print "Mode        %Ekin distribution on the atoms"
-          for i, ev  in enumerate(eigvectors):
+         # we don't know if they are cartesian
+         print "The corresponding eigenvectors  are:"
+         print "Number   Vector"
+         for i, ev  in enumerate(eigvectors):
               write("%3d :    " % (i+1)  )
-              ek =  ev *  dot(mass, ev.T)
-              ek = asarray(ek)
-              ek.shape = (-1, 3)
-              for ek_1 in ek:
-                  eks = sum(ek_1)
-                  write("  %7.2f" % (eks * 100))
-              write("\n" )
+              for j in range(int(len(ev)/3)):
+                   for k in [0,1,2]:
+                       write("  %10.7f" % (ev[j * 3 + k]))
+                   write("\n         " )
+              for k in range(int(len(ev)/3)*3,len(ev)):
+                   write("  %10.7f" % (ev[k]))
+              write("\n")
+         print "----------------------------------------------------"
+         print "kinetic energy distribution"
+         print "Mode        %Ekin distribution on the atoms"
+         for i, ev  in enumerate(eigvectors):
+             write("%3d :    " % (i+1)  )
+             ek =  ev *  dot(mass, ev.T)
+             ek = asarray(ek)
+             ek.shape = (-1, 3)
+             for ek_1 in ek:
+                 eks = sum(ek_1)
+                 write("  %7.2f" % (eks * 100))
+             write("\n" )
 
 
 def check_eigensolver(a, V1, A, B):
-     if MOREOUT:
-         print "Check the results for the eigensolver:"
-         #print dot(V, dot(A, V.T)) - a * eye(len(a))
-         print "V^TAV -a, maximum value:", (abs(dot(V1.T, dot(A, V1)) - a * eye(len(a)))).max()
-         print "V^TBV -1, maximum value:", (abs(dot(dot(V1.T,B),V1) - eye(len(a)))).max()
+    if MOREOUT:
+        print "Check the results for the eigensolver:"
+        #print dot(V, dot(A, V.T)) - a * eye(len(a))
+        print "V^TAV -a, maximum value:", (abs(dot(V1.T, dot(A, V1)) - a * eye(len(a)))).max()
+        print "V^TBV -1, maximum value:", (abs(dot(dot(V1.T,B),V1) - eye(len(a)))).max()
 
-         print "a=", a
-         print "V[:, 0]=", V1[:, 0]
-         print "V[:, -1]=", V1[:, -1]
+        print "a=", a
+        print "V[:, 0]=", V1[:, 0]
+        print "V[:, -1]=", V1[:, -1]
 
-         print (abs(dot(A, V1) - dot(dot(B, V1), diag(a)))).max()
-         x = dot(dot(transpose(V1), B), V1)
-         print (abs(x - diag(diag(x)))).max()
+        print (abs(dot(A, V1) - dot(dot(B, V1), diag(a)))).max()
+        x = dot(dot(transpose(V1), B), V1)
+        print (abs(x - diag(diag(x)))).max()
 
 
-     assert((abs(dot(V1.T, dot(A, V1)) - a * eye(len(a)))).max() < 1e-8)
-     assert((abs(dot(dot(V1.T,B),V1) - eye(len(a)))).max() < 1e-8)
+    assert((abs(dot(V1.T, dot(A, V1)) - a * eye(len(a)))).max() < 1e-8)
+    assert((abs(dot(dot(V1.T,B),V1) - eye(len(a)))).max() < 1e-8)
 
 
 
