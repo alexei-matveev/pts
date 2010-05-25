09e825a4e02a689b246bd9f79d8409044bd5a63e
nikodem <nikodem@theochem.tu-muenchen.de>
2010-05-25 16:55:47 +0000

    AOF: makes analytical derivatives in quad.py available

--- quat.py
+++ quat.py
@@ -45,8 +45,66 @@ The funciton rotmat(v) is a composition of two:
     >>> (rotmat(random) == qrotmat(uquat(random))).all()
     True
 
+Derivatives:
+Compare the direct calculated derivatives with the ones from
+NumDiff:
+
+    >>> from func import NumDiff
+
+First for the uquat function:
+
+Comparision
+    >>> der1 = NumDiff(uquat)
+    >>> deriv1 = der1.fprime(random)
+
+The _* functions give also the analytical derivative;
+    >>> q, deriv2 = _uquat(random)
+
+    >>> (deriv2 - deriv1 < 1e-10).all()
+    True
+
+The same for the qrotmat function
+    >>> der2 = NumDiff(qrotmat)
+    >>> deriv3 = der2.fprime(q)
+    >>> rot, deriv4 = _qrotmat(q)
+    >>> (deriv3 - deriv4 < 1e-10).all()
+    True
+
+    >>> deriv3x = der2.fprime(qx)
+    >>> rotx, deriv4x = _qrotmat(qx)
+    >>> (deriv3x - deriv4x < 1e-10).all()
+    True
+    >>> deriv3y = der2.fprime(qy)
+    >>> roty, deriv4y = _qrotmat(qy)
+    >>> (deriv3y - deriv4y < 1e-10).all()
+    True
+    >>> deriv3z = der2.fprime(qz)
+    >>> rotz, deriv4z = _qrotmat(qz)
+    >>> (deriv3z - deriv4z < 1e-10).all()
+    True
+
+The two derivatives after one another should be
+the same as the one for the complete (rotmat(v)) function
+    >>> derivall = dot(deriv4, deriv2)
+
+    >>> dern = NumDiff(rotmat)
+    >>> deriv5 = dern.fprime(random)
+    >>> (derivall - deriv5 < 1e-10).all()
+    True
+
+Rotmat has also an analytical derivative:
+    >>> rot2, deriv6 = _rotmat(random)
+    >>> (deriv5 - deriv6 < 1e-10).all()
+    True
+    >>> (derivall - deriv6 < 1e-10).all()
+    True
+
+Just compare that the two rotation matrices given here
+are really the same
+    >>> (rot - rot2 < 1e-10).all()
+    True
 """
-from numpy import asarray, empty, dot, sqrt, sin, cos, abs
+from numpy import asarray, empty, dot, sqrt, sin, cos, abs, array, eye, diag, zeros
 
 def uquat(v):
     """Returns unitary quaternion corresponding to rotation vector "v",
@@ -56,8 +114,15 @@ def uquat(v):
         >>> from numpy import pi
 
         >>> uquat((0., 0., pi/2.))
-        (0.70710678118654757, 0.0, 0.0, 0.70710678118654746)
+        array([ 0.70710678,  0.        ,  0.        ,  0.70710678])
+
+    #  (0.70710678118654757, 0.0, 0.0, 0.70710678118654746)
     """
+    uq, __  = _uquat(v)
+    return uq
+
+
+def _uquat(v):
 
     v = asarray(v)
 
@@ -79,10 +144,23 @@ def uquat(v):
     # real component of the quaternion:
     w = cos(phi/2.)
 
+    # Note: dcos(phi/2)/dv = -sin(phi/2) * 1/2 * (v/|v|)
+    dw = - 1/4. * sinc(phi/2.) * v
+
     # imaginary components:
     x, y, z = v/2. * sinc(phi/2.)
 
-    return (w, x, y, z)
+    # dx/v = sinc(phi/2) * 1/2 * dv0/dv + v0/8 * dsinc(phi/2)/dphi/2 * v
+    # dy/v and dz/v the same way
+    dx = 1/2. * sinc(phi/2.) * array([1, 0, 0]) + v[0]/8. * dsinc_dq_c(phi/2.) * v
+    dy = 1/2. * sinc(phi/2.) * array([0, 1, 0]) + v[1]/8. * dsinc_dq_c(phi/2.) * v
+    dz = 1/2. * sinc(phi/2.) * array([0, 0, 1]) + v[2]/8. * dsinc_dq_c(phi/2.) * v
+
+    q = asarray([w, x, y, z])
+
+    dq = asarray([dw, dx, dy, dz])
+
+    return q, dq
 
 def rotmat(v):
     """Generates rotation matrix based on vector v, whose length specifies 
@@ -114,10 +192,24 @@ def rotmat(v):
 
     return qrotmat(uquat(v))
 
-def qrotmat(q):
+def _rotmat(v):
+   uq, duq = _uquat(v)
+   qrot, dqrot = _qrotmat(uq)
 
+   # rot = qrot(uquat(v))
+   # drot/dv = dqrot/du * duquat(u)/dv
+   return qrot, dot(dqrot, duq)
+
+
+def qrotmat(q):
+    m ,__ = _qrotmat(q)
+    return m
+
+
+def _qrotmat(q):
     assert len(q) == 4 # quaternion!
-    assert abs(dot(q, q) - 1.) < 1e-10 # unitary quaternion!
+    #assert abs(dot(q, q) - 1.) < 1e-10 # unitary quaternion!
+    # not if as function, only knowm from relation, thus not with NumDiff usable
 
     a, b, c, d = q
 
@@ -131,7 +223,28 @@ def qrotmat(q):
          [ 2*b*c + 2*a*d        , a*a - b*b + c*c - d*d, 2*c*d - 2*a*b  ],
          [ 2*b*d - 2*a*c        , 2*c*d + 2*a*b        , a*a - b*b - c*c + d*d ]]
 
-    return asarray(m)
+    # The derivations partial m/partial q_i
+    dmda = 2 * array( [[ a, -d, c],
+             [ d,  a, -b],
+             [-c,  b,  a]])
+
+    dmdb =  2 * array( [[ b,  c,  d],
+             [ c, -b, -a],
+             [ d,  a, -b]])
+
+    dmdc =  2 * array( [[-c,  b,  a],
+             [ b,  c,  d],
+             [-a,  d, -c]])
+
+    dmdd =  2 * array( [[-d, -a,  b],
+             [ a, -d,  c],
+             [ b,  c,  d]])
+
+    dmall = asarray([dmda.T, dmdb.T, dmdc.T, dmdd.T])
+
+    # dm/dl[i,j,k] = dm[j,i]/dk
+
+    return asarray(m), dmall.T
 
 def sinc(x):
     """sinc(x) = sin(x)/x
@@ -159,6 +272,19 @@ def sinc(x):
         #   1e-16 and so are unimportant for double precision arithmetic.
 
         return 1 - x**2/6. + x**4/120. - x**6/5040. + x**8/362880.
+
+def dsinc_dq_c(x):
+    if abs(x) > 0.01:
+         return ( (cos(x) / x**2) - (sin(x) / x**3) )
+    else:
+       #    (%i2) taylor( cos(x)/x**2 - sin(x)/x**3, x, 0, 9);
+       #                              2    4      6        8
+       #                         1   x    x      x        x
+       #    (%o2)/T/           - - + -- - --- + ----- - ------- + . . .
+       #                         3   30   840   45360   3991680
+
+         return - 1/3. + x**2/30. - x**4/840. + x**6/45360. + x**8/3991680.
+
 
 class Quat(object):
     """Minimal quaternions
