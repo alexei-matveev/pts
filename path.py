70677d019a72772f5d3807211f19470013b34d76
hugh.chaffey-millar <hugh.chaffey-millar@ch.tum.de>
2010-06-14 12:24:05 +0000

    Beginnings of adding functionality to path.py

--- path.py
+++ path.py
@@ -139,18 +139,85 @@ You can use more points in the path if you decide so:
            [   0.,    0.],
            [  17.,   82.],
            [ 100.,  100.]])
+
+Begin tests of new functionality added AFTER Alexei refactored original code.
+=============================================================================
+
+Straight path
+
+    >>> path = ((-100., -100.), (100., 100.))
+    >>> p = PathRepresentation(path, beads_count=3)
+    >>> p.generate_beads(update=True)
+    array([[-100., -100.],
+           [   0.,    0.],
+           [ 100.,  100.]])
+
+    >>> round(p.get_bead_separations())
+    array([ 141.,  141.])
+
+    >>> p.beads_count = 5
+    >>> p.generate_beads(update=True)
+    >>> p.get_bead_separations().sum() == 2*(100*100 + 100*100)**(0.5)
+    True
+
+Symmetric parabola
+
+    >>> path = ((-100., 100.), (0., 0.), (100., 100.))
+    >>> p = PathRepresentation(path)
+    >>> p.beads_count = 10
+    >>> _ = p.generate_beads(update=True)
+
+    >>> length, _ = scipy.integrate.quad(lambda x: sqrt(1.0 + (0.02*x)**2), 0., 100.)
+    >>> round(p.get_bead_separations().sum() - 2*length, 6)
+    0.0
+
+Test using rho function that is not continuous...
+
+    >>> path = ((-100., -100.), (100., 100.))
+    >>> its = [0, 0.1, 0.2, 0.25, 0.9, 1]
+
+Construct a rho() made of peicewise constant functions...
+
+    >>> rho = RhoInterval(its)
+    >>> p = PathRepresentation(path, 6, rho)
+    >>> p.generate_beads(update=True)
+    array([[-100., -100.],
+           [ -80.,  -80.],
+           [ -60.,  -60.],
+           [ -50.,  -50.],
+           [  80.,   80.],
+           [ 100.,  100.]])
+
+    >>> seps = p.get_bead_separations()
+    >>> seps / seps.sum()
+    array([ 0.1 ,  0.1 ,  0.05,  0.65,  0.1 ])
+
+    >>> path = ((-100., 100.), (0,0), (0,10), (100., 100.))
+    >>> p = PathRepresentation(path, 6, rho)
+    >>> pts = p.generate_beads(update=True)
+    >>> p.write_gplfile('gp1.txt')
+    >>> p = PathRepresentation(pts, 6, rho, xs=[0, 0.1, 0.2, 0.25, 0.9, 1])
+    >>> p.write_gplfile('gp2.txt')
+    >>> p = PathRepresentation(pts, 6, rho)
+    >>> p.write_gplfile('gp3.txt')
+    >>> p = PathRepresentation(pts, 6, rho, xs=[0, 0.08, 0.2, 0.25, 0.9, 1])
+    >>> p.write_gplfile('gp4.txt')
+
+
+
 """
 
 __all__ = ["Path", "PathRepresentation"]
 
 import scipy.optimize
 import scipy.integrate
+import matplotlib.pyplot as plt
 
 from numpy import linalg, array, dot, sqrt, ones, arange, column_stack
 from numpy import ndarray
 from numpy import asarray
 
-from func import LinFunc, QuadFunc, SplineFunc, Func
+from func import LinFunc, QuadFunc, SplineFunc, Func, RhoInterval
 from func import Integral, Inverse
 
 # simplified logger:
@@ -163,6 +230,9 @@ class Path(Func):
     """
 
     def __init__(self, ys, xs=None):
+
+
+        # FIXME: check all beads have same dimensionality
 
         # number of path nodes that define a path:
         self.__node_count = len(ys)
@@ -188,7 +258,8 @@ class Path(Func):
         # using the normalized positions chosen above for the respective
         # points from the state vector:
 
-        self.__regen_path_func()
+        # removed 12/05/2010 by HCM, now called by set_nodes() above.
+        # self.__regen_path_func()
 
         #
         # This function appeared idempotent, so calling it more than one time
@@ -249,11 +320,30 @@ class Path(Func):
 
 #   self.__call__() equivalent to self.f() is inherited form Func()
 
+    def write_gplfile(self, fn):
+        f = open(fn, 'w')
+        ss = arange(0., 1., 1e-3)
+
+        if False:
+            fs = array([self.f(s) for s in ss])
+            plt.plot(fs[:,0], fs[:,1], '-')
+
+        for s in ss:
+            f.write('\t'.join(['%f' % num for num in self.f(s)]))
+            f.write('\n')
+        f.write('\n\n')
+
+        for i in self.nodes:
+            f.write('\t'.join(['%f' % num for num in i]))
+            f.write('\n')
+        f.close()
+           
+
     def get_nodes(self):
         # in the original shape:
         return self.__ys.reshape((self.__node_count,) + self.__yshape)
 
-    def set_nodes(self, ys):
+    def set_nodes(self, ys, xs=None):
         """So far does not accepts spacing of nodes in primary (spline) coordinate,
         only the node positions |ys|. Because it is a property handler.
         """
@@ -264,8 +354,15 @@ class Path(Func):
         # will reference original array if the type matches:
         ys = asarray(ys)
 
+        if xs != None:
+            xs = array(xs)
+            assert len(xs) == len(ys)
+            self.__xs = xs
+
         # first dimension is the node count:
-        assert self.__node_count == len(ys), "%d %d" % (self.__node_count, len(ys))
+        # HCM: changed 12/05/2010, nodes count now comes from 
+        # assert self.__node_count == len(ys), "%d %d" % (self.__node_count, len(ys))
+        self.__node_count = len(ys)
 
         # save original shape of the input arrays:
         self.__yshape = ys[0].shape
@@ -284,7 +381,7 @@ class Path(Func):
         # points from the state vector:
 
         # FIXME: uncommenting this will FAIL the last doctest:
-        # self.__regen_path_func()
+        self.__regen_path_func()
 
         #
         # This function appeared idempotent, so calling it more than one time
@@ -296,6 +393,7 @@ class Path(Func):
         # to be regenerated. This leaves the parent Path class in a somewhat
         # inconsistent state.
         #
+        # HCM: this is probably undesirable. I think I will change this.
 
     nodes = property(get_nodes, set_nodes)
 
@@ -361,18 +459,25 @@ class PathRepresentation(Path):
     spline, depending on whether it has 2, 3 or > 3 points.
     """
 
-    def __init__(self, state_vec, beads_count = None, rho = lambda x: 1):
+    def __init__(self, state_vec, beads_count = None, rho = lambda x: 1, xs=None):
 
-        # construct the spline paramtrization of the path through the nodes
-        # in the |state_vec|:
-        Path.__init__(self, state_vec)
-
-        # number of vectors defining the path
         if beads_count is None:
             # default to the number of nodes:
             self.beads_count = len(state_vec)
         else:
             self.beads_count = beads_count
+
+            # If the specified beads count is different to the number of input 
+            # beads in state_vec, build a new state_vec containing the required 
+            # number of beads.
+            if beads_count != len(state_vec):
+                p = PathRepresentation(state_vec)
+                p.beads_count = beads_count
+                state_vec = p.generate_beads()
+
+        # construct the spline paramtrization of the path through the nodes
+        # in the |state_vec|:
+        Path.__init__(self, state_vec, xs=xs)
 
         self.__path_tangents = []
 
@@ -446,7 +551,13 @@ class PathRepresentation(Path):
         #
 
 
-        # TODO check all beads have same dimensionality
+        # Set the first time generate_beads() is called.
+        # Based on rho().
+        self._normd_positions = None
+        self._old_normd_positions = None
+
+        # temporatry wrapper for Path.nodes property.
+        self.state_vec = self.nodes
 
     def get_fs(self):
         return self.__fs
@@ -454,6 +565,22 @@ class PathRepresentation(Path):
     @property
     def path_tangents(self):
         return self.__path_tangents
+
+    # Extra functions needed for drop-in usage in GrowingString method
+    def path_pos(self):
+        return (self._normd_positions, self._old_normd_positions)
+
+    def get_bead_separations(self):
+        N = self.beads_count
+        arcs = [self.arc(self._normd_positions[i]) for i in range(N)]
+        arc_pairs = [(arcs[i], arcs[i+1]) for i in range(N)[:-1]]
+        seps = [arc2 - arc1 for arc1, arc2 in arc_pairs]
+        return array(seps)
+
+    def update_tangents(self):
+        # This function should hopefully become redundant
+        pass
+
 
     def generate_beads(self, update = False):
         """Returns an array of the self.__beads_count vectors of the coordinates 
@@ -478,6 +605,9 @@ class PathRepresentation(Path):
         bead_tangents = array([self.tangent(0)] + bead_tangents + [self.tangent(1)])
 
         if update:
+
+            self._old_normd_positions = self._normd_positions
+            self._normd_positions = [0.] + normd_positions + [1.]
             #
             # I think there is something tricky here: bead positions and
             # bead tangents here were generated using the "old" path prametrization
@@ -495,7 +625,7 @@ class PathRepresentation(Path):
             # currently this assumes to update the nodes of the Path but not
             # to generate a new Path parametrization. Are we misusing parent class
             # for bead storage here?
-            self.set_nodes(bead_vectors)
+            self.set_nodes(bead_vectors, self._normd_positions)
             #
             # So, when is appropriate moment to reparametrize the path (regen_path_func())?
             # Should it be initiated from the |state_vec| handler?
@@ -531,9 +661,11 @@ class PathRepresentation(Path):
 
         # full path length:
         arc = self.arc(1.0)
+        self.path_len = arc
 
         # full path weight:
-        weight = self.s2w(arc)
+        s2w = Integral(lambda s: self.__rho(s / arc))
+        weight = s2w(arc) #self.s2w(arc)
 
         # shortcut for number of beads:
         n = self.beads_count
@@ -542,7 +674,8 @@ class PathRepresentation(Path):
         weights = [ i * weight / (n - 1) for i in range(1, n - 1) ]
 
         # NOTE: these are unnormalized path arcs:
-        arcs = [ self.w2s(w) for w in weights ]
+        w2s = Inverse(s2w)
+        arcs = [ w2s(w) for w in weights ]
 
         #  desired fractional positions along the string
         return [ self.arg(s) for s in arcs ]
