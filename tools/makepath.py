ee2eaeb53fe542880169266ed7ad810b7c64baa4
nikodem <nikodem@theochem.tu-muenchen.de>
2010-07-08 14:44:12 +0000

    AOF: makepath can work with internal and cartesian coordinates

--- tools/makepath.py
+++ tools/makepath.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
 
 """
-Script to interpolate between two minima in internal
-coordinates, ase and aof needs to be known when running
-The input can be in any ase format, the third parameter
-should be the zmatrix (can be generated in ag),
+Script to interpolate between two minima
+in internal or cartesian coordinates,
+ase and aof needs to be known when running
+The input can be in any ase format, in the files GEOL
+and GEOR
 
-which looks like:
+usage: makepath.py  GEOL GEOR
+
+or
+   makepath.py --num 12 GEOL GEOR
+
+--num gives the number of interpolated points, default
+would be 7
+
+if interpolation is wanted in internal coordinates,
+add --zmat ZMAT
+where the zmatrix in ZMAT (can be generated in ag),
+should look soemthing like:
 "
 Pt
 Pt 1 var1
@@ -32,17 +44,14 @@ The last part giving the initial values for the variables
 needn't be there, but then the variables have to be named
 var1 -> varN
 
-If no parameter is set the output will in xyz format to the
+some additional paramters contain the output:
+If none of them  is set the output will in xyz format to the
 stdout
-
-There is also the possibility for some paramters, which could
-be set after this three files:
-  pos:      the output will be given as poscars in (direct)
+  --pos:      the output will be given as poscars in (direct)
             style as pos0 to posN
-  allcoord: This way all the coordinates in internal and
+  --allcoord: This way all the coordinates in internal and
             Cartesian will be given to the stdout
-  n:        Any number given as last input parameter will be
-            the number of beads to be taken, default is 7
+            (in Cartesian interpolation they are the same)
 """
 
 import sys
@@ -51,81 +60,111 @@ if sys.argv[1] == '--help':
     sys.exit()
 
 from ase import read, write
-from aof.coord_sys import ZMatrix2, RotAndTrans
+from aof.coord_sys import ZMatrix2, RotAndTrans, XYZ
 from aof.path import Path
 from aof.common import file2str
 from numpy import linspace, round, pi, asarray, array
 from string import count
 from pydoc import help
+from aof.inputs.pathsearcher import fake_xyz_string
 
 # Defaultvalues for parameters
 # output as xyz, and 7 beads
 output = 0
 steps = 7
-#the two files for the minima
-m1  = read(sys.argv[1])
-m2  = read(sys.argv[2])
-#Then read in the zmatrix in ag form
-zmts = file2str(sys.argv[3])
 
-# look if there are some more parameters set
-try:
-    if sys.argv[4] == "pos":
+args = sys.argv[1:]
+
+zmts = None
+m1 = None
+
+for k in range(len(args)):
+    if args == []:
+         break
+    if args[0] == "--pos":
          output = 1
-         print "set POSCAR's as output!"
-         steps = int(sys.argv[5])
-    elif sys.argv[4] == "allcoord":
+         #print "set POSCAR's as output!"
+         args = args[1:]
+    elif args[0] == "--allcoord":
          output = 2
-         print "output is a pathfile!"
-         steps = int(sys.argv[5])
-    else:
-        steps = int(sys.argv[4])
-except :
-    pass
+         #print "output gives all coordinates!"
+         args = args[1:]
+    elif args[0] == "--num":
+         steps = int(args[1])
+         #print "interpolation of %d beads" % (steps)
+         args = args[2:]
+    elif args[0] == "--zmat":
+#Then read in the zmatrix in ag form
+         zmts = file2str(args[1])
+         #print "interpolation in internal coordinates"
+         args = args[2:]
+         # The variables in the zmat may not be set yet
+         #then do this as service
+         elem_num = count(zmts, "var")
+         #print elem_num
+         if elem_num > 0:
+             a1, a2, a3 = zmts.partition("var%d" % elem_num)
+             if len(a2) > 0:
+                 zmts += "\n"
+                 for i in range(1,elem_num + 1):
+                     zmts += "   var%d  1.0\n" % i
+#the two files for the minima
+    elif m1 == None:
+         m1 = read(args[0])
+         args = args[1:]
+    else :
+         m2 = read(args[0])
+         args = args[1:]
 
-# The variables in the zmat may not be set yet
-#then do this as service
-elem_num = count(zmts, "var")
-#print elem_num
-if elem_num > 0:
-    a1, a2, a3 = zmts.partition("var%d" % elem_num)
-    if len(a2) > 0:
-        zmts += "\n"
-        for i in range(1,elem_num + 1):
-            zmts += "   var%d  1.0\n" % i
+assert m1 != None
+assert m2 != None
 
 # mol is the ase-atom object to create the ase output
 # the xyz or poscar files
 mol = m1
 
-# internal coordinates are in a ZMatrix2 object
-# Two for the minima, they can be set already
-zmt1 = ZMatrix2(zmts, RotAndTrans())
-zmt1.set_cartesians(m1.get_positions())
-zmt2 = ZMatrix2(zmts, RotAndTrans())
-zmt2.set_cartesians(m2.get_positions())
-# The next one is the one for the current geometry
-zmtinter = ZMatrix2(zmts, RotAndTrans())
+if zmts == None:
+    # Cartesian coordinates are in a ZMatrix2 object
+    # Two for the minima, they can be set already
+    zmt1 = XYZ( fake_xyz_string(m1))
+    zmt1.set_cartesians(m1.get_positions())
+    zmt2 = XYZ( fake_xyz_string(m2))
+    zmt2.set_cartesians(m2.get_positions())
+    # The next one is the one for the current geometry
+    zmtinter = XYZ( fake_xyz_string(m1))
+else:
+    # internal coordinates are in a ZMatrix2 object
+    # Two for the minima, they can be set already
+    zmt1 = ZMatrix2(zmts, RotAndTrans())
+    zmt1.set_cartesians(m1.get_positions())
+    zmt2 = ZMatrix2(zmts, RotAndTrans())
+    zmt2.set_cartesians(m2.get_positions())
+    # The next one is the one for the current geometry
+    zmtinter = ZMatrix2(zmts, RotAndTrans())
 
 # redefine symbols to contain only geometries (internal):
 m1 = zmt1.get_internals()
 m2 = zmt2.get_internals()
 
-# interpolation of dihedral angles needs more logic,
-# say for interpolation from -pi+x to pi-y with small x and y:
-for i, k in enumerate(zmt1.kinds):
-   if k == "dih":
-       delta = m2[i] - m1[i]
-       # normalize the interval between two angles:
-       while delta >  pi: delta -= 2.0 * pi
-       while delta < -pi: delta += 2.0 * pi
-       m2[i] = m1[i] + delta
+if zmts != None:
+    # interpolation of dihedral angles needs more logic,
+    # say for interpolation from -pi+x to pi-y with small x and y:
+    for i, k in enumerate(zmt1.kinds):
+       if k == "dih":
+           delta = m2[i] - m1[i]
+           # normalize the interval between two angles:
+           while delta >  pi: delta -= 2.0 * pi
+           while delta < -pi: delta += 2.0 * pi
+           m2[i] = m1[i] + delta
 
-# path between two minima (linear in internals):
+# path between two minima (linear in coordinates):
 ipm1m2 = Path([m1, m2])
 
 if output == 2:
-    print "Interpolation between M1 and M2 in internals:"
+    if zmts == None:
+        print "Interpolation between M1 and M2 in Cartesians:"
+    else:
+        print "Interpolation between M1 and M2 in internals:"
 
 for i, x in enumerate(linspace(0., 1., steps)):
    if output == 2:
@@ -147,6 +186,6 @@ for i, x in enumerate(linspace(0., 1., steps)):
    # coordinates are written out in xyz or poscar format
    # for the last make sure, that the cell is set in the inputs
    if output == 1:
-       write("pos%i" % i, mol, format="vasp", direct = "True")
+       write("POSCAR%i" % i, mol, format="vasp", direct = "True")
    elif output == 0:
-       write('-'  , mol)
+       write("-" , mol)
