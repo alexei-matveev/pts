2a4f9f28e66036272180c38337d6a4a76a535ac5
nikodem <nikodem@theochem.tu-muenchen.de>
2011-02-11 15:41:45 +0000

    AN: some rearangements in metric module
    * use __str__ instead of version
    * simplify docstring
    * allow Default metric and No_metric without function

--- metric.py
+++ metric.py
@@ -1,6 +1,7 @@
 from func import NumDiff
 from numpy import dot, asarray, matrix, size
 from numpy import sqrt
+from numpy import zeros, eye
 from numpy.linalg import solve
 from copy import deepcopy
 
@@ -17,27 +18,25 @@ class Default:
     This variant supposes that contra- and covariant vectors
     are the same
 
-    Use a real function:
-    >>> from quat import r3
-    >>> met = Default(r3)
+    >>> met = Default(None)
     >>> from numpy import pi
 
-    >>> x_r3 = asarray([8., pi/2. - 0.3, 0.2])
+    >>> x = None
 
-    >>> f_con = asarray([0.0002, 0.001, 0.001])
+    >>> dx_con = asarray([0.0002, 0.001, 0.001])
 
     First transformation
-    >>> f_co = met.lower(f_con, x_r3)
+    >>> dx_co = met.lower(dx_con, x)
 
     Verify: this metric changes nothing:
-    >>> max(abs(f_con - f_co)) < 1e-15
+    >>> max(abs(dx_con - dx_co)) < 1e-15
     True
 
     Verify: this metric changes nothing:
-    >>> max(abs(f_con - met.raises(f_co, x_r3))) < 1e-15
+    >>> max(abs(dx_con - met.raises(dx_co, x))) < 1e-15
     True
     """
-    def __init__(self, fun):
+    def __init__(self, fun = None):
         """
         normally needs a function with a fprime function
         in this special case it is only asked for it because
@@ -52,17 +51,15 @@ class Default:
         return deepcopy(vec)
 
     def norm_up(self, vec, place):
-        vec_con = deepcopy(vec)
-        vec_co = self.lower(vec_con, place)
-        return sqrt(dot(vec_con, vec_co))
+        vec_co = self.lower(vec, place)
+        return sqrt(dot(vec, vec_co))
 
     def norm_down(self, vec, place):
-        vec_co = deepcopy(vec)
-        vec_con = self.raises(vec_co, place)
-        return sqrt(dot(vec_con, vec_co))
+        vec_con = self.raises(vec, place)
+        return sqrt(dot(vec_con, vec))
 
-    def version(self):
-        print "Metric: Working with Metric direct internals (Default)"
+    def __str__(self):
+        return "Metric: Working with Metric direct internals (Default)"
 
 class Metric(Default):
     """
@@ -70,74 +67,34 @@ class Metric(Default):
     transforming contra- and covariant vectors into
     each other
 
-    >>> from numpy import sin, cos, pi
-
-    First test that a lowered raised indice is the same is
-    a not transformed one:
-
-    Functions: Identity function and a "pseudo" atoms function
-    >>> def fun1(x):
-    ...     return deepcopy(x)
-
-    >>> def fun2(x):
-    ...     return asarray([[0., 0., 0.], [x[0], 0., 0.],
-    ...                    [x[1] * sin(x[2]), x[1] * cos(x[2]), 0.],
-    ...                    [x[1] * sin(x[2]) + x[3] * sin(x[4]) * cos(x[5]) ,
-    ...                     x[1] * cos(x[2]) + x[3] * cos(x[4]) * cos(x[5]) ,
-    ...                     - x[3] * sin(x[5])]])
-
-    Be careful that the angles are not too large
-    >>> x = asarray([1.0, 2.0, 0.5, 1.0 , 0.6 , 0.1])
-
-    Vector to transform
-    >>> f_co = asarray([1.0, 2.0, 0.5, 1.0 , 0.6 , 0.1])
-
-    Identity function:
-    >>> met = Metric(fun1)
-
-    First transformation
-    >>> f_con = met.raises(f_co, x)
-
-    Verify: back transformation == start
-    >>> max(abs(f_co - met.lower(f_con, x))) < 1e-15
-    True
-
-    Pseudo atoms:
-    >>> met = Metric(fun2)
-
-    First transformation
-    >>> f_con = met.raises(f_co, x)
-
-    Verify: back transformation == start
-    >>> max(abs(f_co - met.lower(f_con, x))) <  1e-15
-    True
+    >>> from numpy import pi
 
     Use a real function:
     >>> from quat import r3
     >>> met = Metric(r3)
 
-    >>> x_r3 = asarray([8., pi/2. - 0.3, 0.2])
+    >>> x = asarray([8., pi/2. - 0.3, 0.2])
 
-    >>> f_con = asarray([0.0002, 0.001, 0.001])
+    >>> dx_con = asarray([0.0002, 0.001, 0.001])
 
-    >>> x_r3_e = x_r3 + f_con
+    >>> x2 = x + dx_con
 
     First transformation
-    >>> f_co = met.lower(f_con, x_r3)
+    >>> dx_co = met.lower(dx_con, x)
 
     Verify: back transformation == start
-    >>> max(abs(f_con - met.raises(f_co, x_r3))) < 1e-15
+    >>> max(abs(dx_con - met.raises(dx_co, x))) < 1e-15
     True
 
-    >>> abs(dot(f_con, f_co) - dot(r3(x_r3)-r3(x_r3_e), r3(x_r3)-r3(x_r3_e))) < 1e-6
+    >>> abs(dot(dx_con, dx_co) - dot(r3(x)-r3(x2), r3(x)-r3(x2))) < 1e-6
     True
 
     Use norm function of metric (attention, they give back the sqrt)
 
-    >>> abs(met.norm_up(f_con, x_r3) - sqrt(dot(r3(x_r3)-r3(x_r3_e), r3(x_r3)-r3(x_r3_e)))) < 1e-6
+    >>> abs(met.norm_up(dx_con, x) - sqrt(dot(r3(x)-r3(x2), r3(x)-r3(x2)))) < 1e-6
     True
 
-    >>> abs(met.norm_down(f_co, x_r3) - sqrt(dot(r3(x_r3)-r3(x_r3_e), r3(x_r3)-r3(x_r3_e)))) < 1e-6
+    >>> abs(met.norm_down(dx_co, x) - sqrt(dot(r3(x)-r3(x2), r3(x)-r3(x2)))) < 1e-6
     True
     """
     def __init__(self, fun):
@@ -146,7 +103,7 @@ class Metric(Default):
         """
         self.fun = NumDiff(fun)
 
-    def __fprime_as_matrix(self, x):
+    def _fprime_as_matrix(self, x):
         """
         Some functions return not linear arrays of different shapes.
         Then fprime will be not a 2-dimensional array but more.
@@ -160,22 +117,24 @@ class Metric(Default):
         return a
 
     def lower(self, vec, place):
-        return asarray(contoco(self.__fprime_as_matrix, place, vec))
+        return asarray(contoco(self._fprime_as_matrix, place, vec))
 
     def raises(self, vec, place):
-        return asarray(cotocon(self.__fprime_as_matrix, place, vec))
+        return asarray(cotocon(self._fprime_as_matrix, place, vec))
 
-    def version(self):
-        print "Metric: Working with Metric Cartesians (Metric)"
+    def __str__(self):
+        return "Metric: Working with Metric Cartesians (Metric)"
+
 
 
 class No_metric():
-    def __init__(self, fun):
+    def __init__(self, fun = None):
         pass
 
-    def version(self):
-        print "Metric: No metric set! Please initalize a valid metric before using it"
-        print "This metric does not provide any other functionalities"
+    def __str__(self):
+        s =  "Metric: No metric set! Please initalize a valid metric before using it."
+        s = s + '\n' + "This metric does not provide any other functionalities"
+        return s
 """
 The chosen metric, available are:
    * Default, for which contra- and covariant vectors are the same
@@ -194,9 +153,9 @@ but use for example
  co_vec = mt.lower(con_vec)
 """
 global metric
-metric = No_metric
+metric = No_metric()
 
-def setup_metric(F):
+def setup_metric(F = None):
      """
      sets and initalises the metric
      F should be a function, which should when run by
@@ -207,7 +166,7 @@ def setup_metric(F):
      """
      global metric
      metric = Default(F)
-     #metric.version()
+     print metric
 
 def contoco(F, pos, vec):
      """
