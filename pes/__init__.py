92781a978df18d3060c759185024b3054c19b192
matveev <matveev@theochem.tu-muenchen.de>
2011-02-17 08:49:12 +0000

    AM: metric should have a reasonable default, rm unised imports

--- pes/__init__.py
+++ pes/__init__.py
@@ -1,14 +1,8 @@
 import time
 import random
-
 import numpy as np
-
 import mueller_brown as mb
-from plot import Plot2D
 import pts
-from copy import deepcopy
-import pts.metric as mt
-from pts.metric import setup_metric
 
 class QCDriver:
     def __init__(self, dimension):
@@ -81,12 +75,6 @@ class GaussianPES():
         >>> x.energy([0., 1.0])
         """
         self.fake_delay = fake_delay
-        # we need an metric object, this one should
-        # lower and raise indices without changing anything
-        def identity(x):
-            return deepcopy(x)
-        setup_metric(identity)
-        #mt.metric.version()
 
     def __str__(self):
         return "GaussianPES"
