1146bf04369d6fad7b1cde7aa2b7ad0a2836f2b1
matveev <matveev@theochem.tu-muenchen.de>
2011-02-24 12:48:20 +0000

    AM: example use of alternative optimizer

--- test/newcoords/test_mb.py
+++ test/newcoords/test_mb.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
+from pts.path import Path
 from pts.inputs.pathsearcher import find_path
-from numpy import array
+from numpy import array, linspace
 from sys import argv
 from pts.mueller_brown import MB, CHAIN_OF_STATES
 """
@@ -16,19 +17,36 @@ By changing the init_path different (parts) of
 the path can be examined.
 """
 
-bn = int(argv[1])
+beads_count = int(argv[1])
 
 # The tree minima
 min1 = CHAIN_OF_STATES[0] # array([-0.55822362,  1.44172583])
 min2 = CHAIN_OF_STATES[2] # array([-0.05001084,  0.46669421])
 min3 = CHAIN_OF_STATES[4] # array([ 0.62349942,  0.02803776])
 
-# starting path
+# Three minima:
 init_path = [min1, min2, min3]
 
-# the complete run
-conv, path, es, gs = find_path(MB, init_path, ftol = 0.001, maxit = 100, beads_count = bn)
+#
+# Initial guess:
+#
+if beads_count != len(init_path):
+    p = Path(init_path)
+    init_path = map(p, linspace(0., 1., beads_count))
 
-print "energies=\n", es
-print "converged=\n", conv
+#
+# Search by default method:
+#
+res1 = find_path(MB, init_path, ftol = 0.001, maxit = 100)
+
+#
+# Search by an alternative method:
+#
+res2 = find_path(MB, init_path, ftol = 0.001, maxit = 100, method="sopt")
+
+print "\n"
+print "result 1=\n", res1[0], "\n", res1[1], "\n", res1[2], "\n", res1[3]
+print "\n"
+print "result 2=\n", res2[0], "\n", res2[1], "\n", res2[2], "\n", res2[3]
+
 
