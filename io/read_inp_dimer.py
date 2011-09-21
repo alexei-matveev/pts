8e369a7825ea810446b518c58ced643e133908e8
nikodem <nikodem@theochem.tu-muenchen.de>
2011-09-21 03:49:35 +0000

    AN: add dimer help text

--- io/read_inp_dimer.py
+++ io/read_inp_dimer.py
@@ -1,6 +1,53 @@
 #!/usr/bin/env python
+"""
+This function performs a transition state search with the "dimer method"
+
+USAGE:
+   paratools dimer --calculator <calculator_file> <geometry file> <start mode_vector file>
+
+It needs at least three input files:
+ * one containing the quantum chemical calculator
+   (<calculator_file>) which is given via the parameter --calculator
+ * one containing some kind of Cartesian geometry. This file (<geometry file>) should be
+   ASE readable, meaning being in a format that ASE could understand (or be a gxfile having
+   gx in its name)
+ * one should contain a mode vector. This should be as near as possible to the expected
+   negative eigenmode at the position specified by the geometry file. It can be given in internal
+   coordinates (than each entry needs a new line) or in Cartesian coordinates (where each atom has
+   a line with three entries).
+
+GEOMETRIES AND COORDINATE SYSTEMS:
+
+There are more options to the coordinate system choice (for example using internal coordinates for
+optimization). To find more about them do:
+   paratools dimer --help geometries
+
+FURTHER OPTIONS:
+
+There are some more variables specifying the way the dimer method run. Currently they are just handed over
+to the dimer main program, so one could use them but the interface to them is not yet finished as
+is the set of them. The defaults are currently similar to the ones given by ASE dimer, which follows a
+very similar strategy. If it is wanted to change there something find out the name of the variable to change
+in dimer module and add to your command:
+  --<variable name> <new value>
+
+Just to mention one: the maximum number of translation step defaults to a very large number. to reduce it
+add to the paratools command:
+  --max_translation <number of translation iterations>
+
+So for example to have only 100 of them:
+  --max_translation 100
+
+Currently there are maximal 10 rotation steps per translation step allowed.
+EXAMPLES:
+Having in a directory the geometry file (POSCAR) POSCAR.start as well as the mode vector file MODE_START
+and a valid caluclator file calc.py do to start a 20 translation steps dimer optimization:
+
+  paratools dimer --calculator calc.py  --max_translation 20 POSCAR.start MODE_START
+
+"""
 from pts.io.read_inputs import get_geos, get_masked, from_params_file
-from pts.io.read_COS import geo_params
+from pts.io.read_COS import geo_params, info_geometries
 from numpy import loadtxt
 from pts.func import compose
 from pts.qfunc import QFunc
@@ -18,6 +65,15 @@ def read_dimer_input(rest):
     zmatrix = []
     geo = None
     mode = None
+
+    if "--help" in rest:
+        if "geometries" in rest:
+           info_geometries()
+        else:
+           print __doc__
+
+        exit()
+
 
     for i in range(len(rest)):
         if rest == []:
