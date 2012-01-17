660ea3aae38c4c43cba41147a31d3fc85d4875b6
matveev <matveev@theochem.tu-muenchen.de>
2012-01-17 12:13:20 +0000

    AM: move docstring to class body
    - due to the import from the future the module docstring
      is not recognized as such

--- qfunc.py
+++ qfunc.py
@@ -1,49 +1,12 @@
 #!/usr/bin/env python
 from __future__ import with_statement # need to be at the beginning
 """
-    >>> from ase import Atoms
-    >>> ar4 = Atoms("Ar4")
+FIXME:  the line  above  apparently leads  to  this string  not to  be
+interpreted as  a docstring.  Do NOT put  doctests here, they  are not
+verified:
 
-This uses LennardJones() as default calculator:
-
-    >>> pes = QFunc(ar4)
-
-Provide a different one by specifying the second
-argument, e.g: pes = QFunc(ar4, gaussian)
-
-    >>> from numpy import array
-    >>> x = array([[  1.,  1.,  1. ],
-    ...            [ -1., -1.,  1. ],
-    ...            [  1., -1., -1. ],
-    ...            [ -1.,  1., -1. ]])
-
-    >>> pes(x)
-    -0.046783447265625
-
-    >>> pes.fprime(x)
-    array([[ 0.02334595,  0.02334595,  0.02334595],
-           [-0.02334595, -0.02334595,  0.02334595],
-           [ 0.02334595, -0.02334595, -0.02334595],
-           [-0.02334595,  0.02334595, -0.02334595]])
-
-    >>> from numpy import linspace
-    >>> [ (scale, pes(scale * x)) for scale in linspace(0.38, 0.42, 3) ]
-    [(0.38, -5.469484020549146), (0.40000000000000002, -5.9871235862374306), (0.41999999999999998, -5.5011134098626151)]
-
-Find the minimum (first scale the cluster by 0.4 which is close
-to the minimum):
-
-    >>> from fopt import minimize
-    >>> x = x * 0.4
-    >>> xm, fm, _ = minimize(pes, x)
-    >>> round(fm, 7)
-    -6.0
-    >>> xm
-    array([[ 0.39685026,  0.39685026,  0.39685026],
-           [-0.39685026, -0.39685026,  0.39685026],
-           [ 0.39685026, -0.39685026, -0.39685026],
-           [-0.39685026,  0.39685026, -0.39685026]])
-
+    >>> 2 * 2
+    5
 """
 
 __all__ = ["QFunc"]
@@ -56,6 +19,51 @@ from shutil import copy2 as cp
 import numpy as np
 
 class QFunc(Func):
+    """
+        >>> from ase import Atoms
+        >>> ar4 = Atoms("Ar4")
+
+    This uses LennardJones() as default calculator:
+
+        >>> pes = QFunc(ar4)
+
+    Provide a different one by  specifying the second argument, e.g: pes =
+    QFunc(ar4, gaussian)
+
+        >>> from numpy import array
+        >>> x = array([[  1.,  1.,  1. ],
+        ...            [ -1., -1.,  1. ],
+        ...            [  1., -1., -1. ],
+        ...            [ -1.,  1., -1. ]])
+
+        >>> pes(x)
+        -0.046783447265625
+
+        >>> pes.fprime(x)
+        array([[ 0.02334595,  0.02334595,  0.02334595],
+               [-0.02334595, -0.02334595,  0.02334595],
+               [ 0.02334595, -0.02334595, -0.02334595],
+               [-0.02334595,  0.02334595, -0.02334595]])
+
+        >>> from numpy import linspace
+        >>> [pes(scale * x) for scale in linspace(0.38, 0.42, 3)]
+        [-5.469484020549146, -5.9871235862374306, -5.5011134098626151]
+
+    Find the minimum (first scale the cluster by 0.4 which is close to the
+    minimum):
+
+        >>> from fopt import minimize
+        >>> x = x * 0.4
+        >>> xm, fm, _ = minimize(pes, x)
+        >>> round(fm, 7)
+        -6.0
+        >>> xm
+        array([[ 0.39685026,  0.39685026,  0.39685026],
+               [-0.39685026, -0.39685026,  0.39685026],
+               [ 0.39685026, -0.39685026, -0.39685026],
+               [-0.39685026,  0.39685026, -0.39685026]])
+
+    """
     def __init__(self, atoms, calc=LennardJones(), moving=None):
 
         # we are going to repeatedly set_positions() for this instance,
