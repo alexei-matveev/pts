21fb6da17a1d61eae1dba57f5b60f463aedc23ce
matveev <matveev@theochem.tu-muenchen.de>
2012-01-20 12:14:32 +0000

    AM: format docstrings in path2xyz

--- tools/path2xyz.py
+++ tools/path2xyz.py
@@ -1,52 +1,60 @@
 #!/usr/bin/env python
 """
-This tools takes a path file
-reads it in and gives a string of
-jmol-type xyz-files to the standartoutput
-path file can be in path.pickle format, else it can be
-given via internal coordinates file with some extra handling
-see below
+This  tools takes  a  path file  reads it  in  and gives  a string  of
+jmol-type  xyz-files  to  the  standartoutput  path  file  can  be  in
+path.pickle format, else it can be given via internal coordinates file
+with some extra handling see below
 
-They are equally spaced in the path coordinate
+They are equally spaced in the path coordinate.
 
-As a default it will give as many pictures as there
-were beads in the path.pickle file, but any number N can be given
+As a default it will give as  many pictures as there were beads in the
+path.pickle file, but any number N can be given
 
 Run:
 
-    $paratools path2xyz some.path.pickle --num N
+    paratools path2xyz some.path.pickle --num N
 
 or simply:
 
-    $paratools path2xyz some.path.pickle
+    paratools path2xyz some.path.pickle
 
-One could also demand just to give back the beads in this format
-(as the beads my differ from the equally in the path coordinate spaced
-points)
-This could be done by:
+One could also  demand just to give back the beads  in this format (as
+the beads  my differ  from the equally  in the path  coordinate spaced
+points). This could be done by:
 
-    $paratools path2xyz some.path.pickle -b
+    paratools path2xyz some.path.pickle -b
 
-Furthermore it is possible to provide the input in separate files, then instead
-of the some.path.pickle file a coordinate file should be provided, including the
-geometry for all the beads. The minimal requirements are this coordinate file and
-a file containing the atoms symbols.
+Furthermore it  is possible  to provide the  input in  separate files,
+then instead of the some.path.pickle  file a coordinate file should be
+provided,  including  the geometry  for  all  the  beads. The  minimal
+requirements are this coordinate file  and a file containing the atoms
+symbols.
 
-    $paratools path2xyz coordinate.file --symbols symbolfile
+    paratools path2xyz coordinate.file --symbols symbolfile
 
-It might be possible that the coordinates are in internal coordinates and at least
-when the path is wanted it is required to build up the internal coordinate transformation
-rather than transforming the coordinates.
-Further options for building up the transformation into Cartesian coordinates
-    (needed if coordinate.file contains internal coordinates and not only flattend Caresians)
-    --zmat zmatrixfile  : adds zmatrix of zmatrixfile to transformation
-    --mask maskfile raw_geom : only variables wich are "True" in mask are supposed to be
-                          contained in coordinate.file, raw_geom has to give complete set
-                          (fixed coordinates will be gotten from there)
+It might be possible that  the coordinates are in internal coordinates
+and at least  when the path is  wanted it is required to  build up the
+internal  coordinate  transformation   rather  than  transforming  the
+coordinates.  Further options for  building up the transformation into
+Cartesian  coordinates (needed  if  coordinate.file contains  internal
+coordinates and not only flattend Caresians)
+
+    --zmat zmatrixfile
+
+        adds zmatrix of zmatrixfile to transformation
+
+    --mask maskfile raw_geom
+
+        only  variables wich  are "True"  in mask  are supposed  to be
+        contained  in coordinate.file, raw_geom  has to  give complete
+        set (fixed coordinates will be gotten from there)
 
 Further options:
-    --abscissa abscissafile : abscissa data can be gotten from here. String calcualations provide
-                            this data.
+
+    --abscissa abscissafile
+
+        abscissa data  can be  gotten from here.  String calcualations
+        provide this data.
 """
 
 from pts.path import Path
@@ -57,9 +65,8 @@ from pts.tools.pathtools import unpickle_path, read_path_fix, read_path_coords
 
 def read_in_path(filename):
     """
-    Reads in a path.pickle object and gives
-    back the informations of interest, e.g. the
-    coordinates and path positions of the beads
+    Reads in a  path.pickle object and gives back  the informations of
+    interest, e.g. the coordinates and path positions of the beads
     """
     try:
         coord, pathps, energy, gradients, symbols, int2cart = unpickle_path(filename)
@@ -85,11 +92,9 @@ def read_in_path_raw(coordfile, symbfile, zmatifiles = None, pathpsfile = None, 
 
 def path_geos(x, y, cs, num):
     """
-    generates  num geometries along path
-    defined by x and y,
-    The geometries are given back as string
-    there is also a second string given back with
-    the corresponding x values
+    generates  num geometries  along  path  defined by  x  and y,  The
+    geometries are given back as  string there is also a second string
+    given back with the corresponding x values
     """
     path1 = Path(y, x)
 
@@ -118,12 +123,9 @@ def path_geos(x, y, cs, num):
 
 def print_xyz(x, y, cs, num):
     """
-    prints num xyz -frames (jmol format)
-    which are equally distributed on the
-    x values of the path
-    If num is not set, there will be
-    as many frames as there have been
-    geometries in y (but need not be at
+    prints num xyz -frames (jmol format) which are equally distributed
+    on the x values of the path.   If num is not set, there will be as
+    many frames as there have been geometries in y (but need not be at
     the same geometries)
     """
     if num is None:
@@ -157,14 +159,11 @@ def print_xyz(x, y, cs, num):
 
 def print_beads(ys, cs):
     """
-    Prints the xyz- geoemtry in jmol format
-    of the beads
-    This does exactly the same as above, but
-    without the calculation of the path, this
-    ensures that not only exactly the number of
-    bead positions is taken but also that it's
-    exactly the beads which are used to create
-    the frames
+    Prints the xyz-  geoemtry in jmol format of  the beads.  This does
+    exactly  the same  as above,  but without  the calculation  of the
+    path,  this ensures  that  not  only exactly  the  number of  bead
+    positions is taken but also  that it's exactly the beads which are
+    used to create the frames
     """
     symbs, int2cart = cs
     numats = len(symbs)
@@ -178,10 +177,8 @@ def print_beads(ys, cs):
 
 def main(argv):
     """
-    Reads in stuff from the sys.argv if not
-    provided an other way
-    set up a path and gives back positions
-    on it
+    Reads in stuff from the sys.argv  if not provided an other way set
+    up a path and gives back positions on it
     """
     if argv[0] == '--help':
         print __doc__
