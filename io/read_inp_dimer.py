ddfc5c4444bea5edd50b30709daba23d16da19f1
zheng <zheng@theochem.tu-muenchen.de>
2012-02-06 11:48:48 +0000

    ZBX:bug fixed in dimer accept_all setting

--- io/read_inp_dimer.py
+++ io/read_inp_dimer.py
@@ -431,6 +431,8 @@ def from_params_file_dimer( lines ):
     # so that the calculators from there can be used
 
     # execute the string, the variables should be set in the locals
+    from pts.io.read_COS import geo_params
+    
     params_dict = {}
     geo_dict = {}
 
