ef436605cecf32c0a08b86ef7f7c837c5650114e
matveev <matveev@theochem.tu-muenchen.de>
2012-09-14 09:21:29 +0000

    AM: format comments

--- cfunc.py
+++ cfunc.py
@@ -1,54 +1,61 @@
 """
-Some classes which might be used as (wrapper for) functions for the
-CoordSys2.
-They are all with fprime /taylor attached as funcs. Be aware that
-they expeced when used as wrappers, that they get also an inner function
-which is a Func.
+Some classes  which might be used  as (wrapper for)  functions for the
+CoordSys2.  They  are all  with fprime /taylor  attached as  funcs. Be
+aware that they  expeced when used as wrappers, that  they get also an
+inner function which is a Func.
 
     >>> from pts.func import NumDiff
     >>> from pts.zmat import ZMat
     >>> from numpy import max, abs, pi
 
-    A example function:
+A example function:
+
     >>> rep = [(None, None, None), (0, None, None), (0, 1, None)]
     >>> fun1 = ZMat(rep)
 
-    A second example:
+A second example:
+
     >>> fun2 = Justcarts()
 
-    Vector for H2O
+Vector for H2O
+
     >>> y = asarray([0.96, 0.96, 104.5 * pi / 180.0])
 
-    Cartesian start vector:
+Cartesian start vector:
+
     >>> x = asarray(fun1(y)).flatten()
     >>> x_raw = fun1(y)
 
-    Test addition of global paramter:
+Test addition of global paramter:
 
     >>> f_wg = With_globals(fun1)
 
     >>> y_wg = zeros(9)
     >>> y_wg[:3] = y
 
-    If the globals are zero, nothing is changed:
+If the globals are zero, nothing is changed:
+
     >>> max(abs(f_wg(y_wg) - fun1(y))) < 1e-12
     True
 
-    Pseudo inverse:
+Pseudo inverse:
+
     >>> max(abs(f_wg(f_wg.pinv(x_raw)) - f_wg(y_wg))) < 1e-12
     True
 
-    Ensure that the derivatives are correct:
-    Consider that NumDiff has size (n,3,m), while we get normally something of
-    size (n*3, m) back.
+Ensure  that the derivatives  are correct:  Consider that  NumDiff has
+size (n, 3, m), while we get normally something of size (n*3, m) back.
+
     >>> f_nd = NumDiff(f_wg)
     >>> max(abs(f_wg.fprime(y_wg) - f_nd.fprime(y_wg))) < 1e-12
     True
 
-    Test adding several functions:
+Test adding several functions:
+
     >>> f_set = Mergefuncs([fun2, fun2, fun2], [3,3,3], dims_rev = [1,1,1])
 
-    Gives the same result than a complete function for all three of them:
+Gives the same result than a complete function for all three of them:
+
     >>> max(abs(f_set(x) - fun2(x))) < 1e-12
     True
     >>> max(abs(f_set.fprime(x) - fun2.fprime(x))) < 1e-12
@@ -58,15 +65,18 @@ which is a Func.
     >>> max(abs(f_set(f_set.pinv(x2)) - f_set(x))) < 1e-12
     True
 
-    Test fixture of coordinates:
+Test fixture of coordinates:
+
     Fix the Angle:
     >>> mask = [True, True, False]
     >>> f_m = Masked(fun1, mask, y)
 
-    Now only two variables are needed as internal coordinates:
+Now only two variables are needed as internal coordinates:
+
     >>> y_red = asarray([0.96, 0.96])
 
-    At the start point still the right values are given:
+At the start point still the right values are given:
+
     >>> max(abs(f_m(y_red) - fun1(y))) < 1e-12
     True
 
@@ -74,19 +84,21 @@ which is a Func.
     >>> max(abs(f_m(f_m.pinv(x2)) - f_m(y_red))) < 1e-12
     True
 
-    Ensure that the derivatives are correct:
+Ensure that the derivatives are correct:
+
     >>> f_nd = NumDiff(f_m)
     >>> max(abs(f_m.fprime(y_red) - f_nd.fprime(y_red))) < 1e-12
     True
 
-    Now have some values repeated:
-    Both bond-length are the same:
+Now have some values repeated: Both bond-length are the same:
+
     >>> mask = [1, 1, 2]
     >>> f_we = With_equals(fun1, mask)
 
     >>> y_r2 =  asarray([0.96, 104.5 * pi / 180.0])
 
-    Still the same result:
+Still the same result:
+
     >>> max(abs(f_we(y_r2) - fun1(y))) < 1e-12
     True
 
@@ -94,18 +106,21 @@ which is a Func.
     >>> max(abs(f_we(f_we.pinv(x))-f_we(y_r2))) < 1e-12
     True
 
-    Ensure that the derivatives are correct:
+Ensure that the derivatives are correct:
+
     >>> f_nd = NumDiff(f_we)
     >>> max(abs(f_we.fprime(y_r2) - f_nd.fprime(y_r2))) < 1e-12
     True
 
-    Both bond-lengt are the same and the angle is fixed:
+Both bond-lengt are the same and the angle is fixed:
+
     >>> mask = [1, 1, 0]
     >>> f_we = With_equals(fun1, mask, y)
 
     >>> y_r2 =  asarray([0.96])
 
-    Still the same result:
+Still the same result:
+
     >>> max(abs(f_we(y_r2) - fun1(y))) < 1e-12
     True
 
@@ -113,7 +128,8 @@ which is a Func.
     >>> max(abs(f_we(f_we.pinv(x))-f_we(y_r2))) < 1e-12
     True
 
-    Ensure that the derivatives are correct:
+Ensure that the derivatives are correct:
+
     >>> f_nd = NumDiff(f_we)
     >>> max(abs(f_we.fprime(y_r2) - f_nd.fprime(y_r2))) < 1e-12
     True
@@ -143,9 +159,9 @@ class Pass_through(Func):
 
 class Justcarts(Func):
     """
-    Takes Cartesian coordinates and returns Cartesian coordinates.
-    It considers only that the internal coordinates are an array, while
-    the Cartesian are supposed to be a Matrix of size (-1, 3)
+    Takes Cartesian coordinates and returns Cartesian coordinates.  It
+    considers only  that the internal coordinates are  an array, while
+    the Cartesian are supposed to be a Matrix of shape N x 3.
     """
     def __init__(self):
         pass
@@ -164,16 +180,17 @@ class Justcarts(Func):
 
 class With_globals(Func):
     """
-    Adds to function fun_raw some global parameter for:
-      rotation (as vector description of quaternion)
-      translation
-      of the complete molecule.
-      The rotation and translation is calculated with rt object.
-      It is supposed that for each internal coordinates given, the
-      last three coordinates are the global translation and the
-      three before that are for the global rotation.
-    After each calculation of the Cartesian coordinates by fun_raw, they
-    are adapted with the global parameter.
+    Adds to  function fun_raw some global parameter  for: rotation (as
+    vector  description  of quaternion)  translation  of the  complete
+    molecule.
+
+    The rotation and translation is  calculated with rt object.  It is
+    supposed that for each  internal coordinates given, the last three
+    coordinates are  the global translation and the  three before that
+    are for the global rotation.
+
+    After each  calculation of  the Cartesian coordinates  by fun_raw,
+    they are adapted with the global parameter.
     """
     def __init__(self, fun_raw):
         self._rt = RT(fun_raw)
@@ -187,7 +204,8 @@ class With_globals(Func):
         w = y[-6:-3]
         t = y[-3:]
 
-        # rt needs zmatrix coordinates, rotation and translation separate
+        # rt  needs  zmatrix  coordinates,  rotation  and  translation
+        # separate
         x, (xv, xw, xt) = self._rt.taylor(v, w, t)
 
         dx = zeros((len(x[:,0]), 3, len(y)))
@@ -198,13 +216,15 @@ class With_globals(Func):
         dx[:,:,-6:-3] = xw
         dx[:,:,-3:] = xt
 
-        # give back x as a 1-dimensional array (rt gave
-        # back as x, y,z compontent for each "atom"
-        # change the derivatives accordingly
+        # give back x as a 1-dimensional array (rt gave back as x, y,z
+        # compontent   for   each   "atom"  change   the   derivatives
+        # accordingly
         x.shape = (-1, 3) # (number of atoms, 3)
-        dx.shape = (-1, 3, len(y)) # ( number of atoms, 3, number internal coordinates)
-        # (-1 means that in this direction the size should be made
-        # fitting to the other specifications (so that the complete
+        dx.shape = (-1, 3, len(y)) # ( number of atoms, 3, number
+                                   # internal coordinates)
+        #
+        # (-1 means  that in  this direction the  size should  be made
+        # fitting to  the other  specifications (so that  the complete
         # array could be transformed))
 
         return x, dx
@@ -221,10 +241,9 @@ class With_globals(Func):
 
 class Mergefuncs(Func):
     """
-    Makes of a set of functions (in list funs_raw) a combined
-    functions.
-    For initalization it also needs to know the dimensions of the
-    needed internal coordinate arrays for the single functions,
+    Makes  of  a  set  of  functions (in  list  funs_raw)  a  combined
+    functions.  For initalization it also needs to know the dimensions
+    of the needed internal coordinate arrays for the single functions,
     as it needs to know where to break the complete internals vector.
     """
     def __init__(self, funs_raw, dims, dims_rev = None):
@@ -237,9 +256,9 @@ class Mergefuncs(Func):
         res = []
         dres = []
         iter = 0
-        # first calculate the results separate for each function
-        # consider here which part of the complete internal coordinate vector
-        # belongs to it (stored in dims)
+        # first  calculate  the  results  separate for  each  function
+        # consider here which part of the complete internal coordinate
+        # vector belongs to it (stored in dims)
         for fun, dim in zip(self._funs_raw, self._dims):
              r1, dr1 = fun.taylor(x[iter:iter + dim])
              res.append(r1.flatten())
@@ -247,14 +266,15 @@ class Mergefuncs(Func):
              iter += dim
 
         assert (iter == len(x))
-        # the Cartesian coordinates have only to be brought to the correct shape
+        # the  Cartesian coordinates have  only to  be brought  to the
+        # correct shape
         res = hstack(res) # add all the coordinates up
         res.shape = (-1, 3) # (number atoms, 3)
 
-        # for the derivatives some more has to be done: as the functions does not
-        # interact which each other, one has fill up the derivatives with zeros
-        # the single derivative results will be put on the diagonals of the
-        # complete matrix
+        # for  the  derivatives some  more  has  to  be done:  as  the
+        # functions does  not interact which each other,  one has fill
+        # up the derivatives with  zeros the single derivative results
+        # will be put on the diagonals of the complete matrix
         lenx = len(res.flatten())
         mat = zeros((lenx, iter))
 
@@ -267,7 +287,8 @@ class Mergefuncs(Func):
              mat[i:i + a, j : j + b] = m1
              i += a
              j += dim
-        mat.shape = (-1, 3, iter) # number atoms, 3, number internal coordinates
+        mat.shape = (-1, 3, iter) # number atoms, 3, number internal
+                                  # coordinates
 
         return res, mat
 
@@ -285,10 +306,10 @@ class Mergefuncs(Func):
 
 class Masked(Func):
     """
-    The initial function fun_raw has also some variables, which have
-    to be fixed. They are put as False in the mask. One has to give
-    only once a complete vector (for_fix) for all other calculations
-    the fixed coordinates of this one are added to the current set of
+    The initial  function fun_raw has also some  variables, which have
+    to be fixed.  They are put as  False in the mask. One  has to give
+    only once  a complete vector (for_fix) for  all other calculations
+    the fixed coordinates of this one  are added to the current set of
     internals before they are handed to the inner function.
     """
     def __init__(self, fun_raw, mask, for_fix):
@@ -323,7 +344,8 @@ class Masked(Func):
 
         # transpose mat back
         mat = mat.T
-        mat.shape = (-1, 3, len(x)) # number atoms, 3, number internal coordinates
+        mat.shape = (-1, 3, len(x)) # number atoms, 3, number internal
+                                    # coordinates
 
         return res, mat
 
@@ -333,12 +355,13 @@ class Masked(Func):
 
 class With_equals(Func):
     """
-    Is a wrapper function around fun_raw which considers that there are
-    several variables with the same value (as given by mask), which have to
-    be given several times to the inner function
-    The mask is here a list of integers. It gives for each wanted internal coordinate
-    of the inner function which value of the internal coordinates given to with_equals
-    should be used (it is supposed to be several times the same one)
+    Is a  wrapper function around  fun_raw which considers  that there
+    are  several variables  with the  same value  (as given  by mask),
+    which have  to be  given several times  to the inner  function The
+    mask is here a list of integers. It gives for each wanted internal
+    coordinate  of the  inner  function which  value  of the  internal
+    coordinates given to with_equals should be used (it is supposed to
+    be several times the same one)
     """
     def __init__(self, fun_raw, mask, for_fix = None):
         self._fun_raw = fun_raw
