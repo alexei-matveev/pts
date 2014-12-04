10e6c6d84e862e7f4b1c81776359b9eaa224aefb
Alexei Matveev <alexei.matveev@gmail.com>
2014-12-04 11:50:48 +0100

    AM: comments and whitespace

--- ui/write_COS.py
+++ ui/write_COS.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 def print_xyz_with_direction(write_method, coord, cs, text = "", direction = None):
     """
-    Prints the geometry coord (after transformed in Cartesian)
-    and the direction into the write_method.
+    Prints the geometry coord (after transformed in Cartesian) and the
+    direction into the write_method.
+
     INPUT:
-    write_method: should be a write method, like fopen(filename, "w") or
-                  std.write.
+    write_method: should be a write method, like fopen(filename, "w").write or
+                  sys.stdout.write.
     coord:        geometries to store (should be in internal coordinates).
     cs:           like others: Symbols of atoms, trafo from internal to Cartesians
     text:         if some special text is been wanted for the otherwise not used line.
@@ -25,7 +26,7 @@ def print_xyz_with_direction(write_method, coord, cs, text = "", direction = Non
 
     else:
         transfer = trafo.fprime(coord)
-        dirs = dot( transfer, direction)
+        dirs = dot (transfer, direction)
         for s, pos, dir in zip(symbs, carts, dirs):
              write_method("%-2s %22.15f %22.15f %22.15f    %12.8f %12.8f %12.8f\n" \
               % (s, pos[0], pos[1], pos[2], dir[0], dir[1], dir[2]))
